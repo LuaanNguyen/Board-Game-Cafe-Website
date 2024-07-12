3f632effad101eb7361d7e1175d03047915a8fc0
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-11 17:39:56 -0700

    Fixed Navbar 💅

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,36 @@
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-
 function Navbar() {
   return (
-    <nav className="w-[100vw] flex items-center justify-between px-10 glassmorphism-effect bg-orangeMain">
-      <img src="/sipnplay.png" className="w-20 mx-2"></img>
-      <div>
-        <ul className="list-none flex gap-x-10 font-roboto text-base font-bold">
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Menu</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Reservation</a>
-          </li>
-          <li>
-            <a href="">Events</a>
-          </li>
-        </ul>
-      </div>
-      <div className="flex gap-x-4 mx-4">
-        <FaInstagram className="w-10 h-10" />
-        <FaFacebook className="w-10 h-10" />
-      </div>
+    <nav className="w-full fixed top-0 left-0">
+      <section className="m-10 bg-orangePale rounded-2xl shadow-xl flex justify-between items-center px-6 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border">
+        <a href="/">
+          <img src="/sipnplay.png" className="w-20 mx-2 bg-transparent"></img>
+        </a>
+        <div className="flex gap-14 font-bold ">
+          <a href="/">
+            <span className="">About</span>
+          </a>
+          <a href="/">
+            <span>MTG schedule</span>
+          </a>
+          <a href="/">
+            <span>Menu</span>
+          </a>
+          <a href="/">
+            <span>Giftcards</span>
+          </a>
+          <a href="/">
+            <span>Feedback</span>
+          </a>
+          <a href="/">
+            <span>Press</span>
+          </a>
+        </div>
+        <button className="rounded-full p-2 border-2 border-black">
+          <a href="/">
+            <span className="font-bold">Reservation</span>
+          </a>
+        </button>
+      </section>
     </nav>
   );
 }
