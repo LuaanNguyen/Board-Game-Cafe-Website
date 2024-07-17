57616d7c165dae8745aacf6767c693204387ff61
Andy Le <4ndyle@gmail.com>
2024-07-17 11:17:15 -0700

    Fix footer to stick to bottom of pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
 function Footer() {
   return (
-    <footer className="bg-white flex flex-col w-[100vw] items-center justify-center border-t-2 mt-10 border-greenAccent">
-      <section className="px-10 w-[100vw] flex pt-6 pb-6 justify-between max-md:justify-center">
-        <h4 className="text-gray-500  text-center">
-          ©2024 by Sip & Play. All Rights Reserved.
-        </h4>
-        <div className="flex gap-10 max-md:hidden ">
-          <h4 className="text-gray-500">Term</h4>
-          <h4 className="text-gray-500">Privacy</h4>
-          <h4 className="text-gray-500">Cookies</h4>
-        </div>
+    <footer className="mt-auto">
+      <section className="bg-white flex flex-col w-[100vw] items-center justify-center border-t-2 mt-10 border-greenAccent">
+        <section className="px-10 w-[100vw] flex pt-6 pb-6 justify-between max-md:justify-center">
+          <h4 className="text-gray-500  text-center">
+            ©2024 by Sip & Play. All Rights Reserved.
+          </h4>
+          <div className="flex gap-10 max-md:hidden ">
+            <h4 className="text-gray-500">Term</h4>
+            <h4 className="text-gray-500">Privacy</h4>
+            <h4 className="text-gray-500">Cookies</h4>
+          </div>
+        </section>
       </section>
     </footer>
   );
